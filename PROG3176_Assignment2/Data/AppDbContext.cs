075b9e5fbd907e4202edc5630e55f26af686b92f
TeaLizard <owentwilliams7@gmail.com>
2026-02-19 17:53:01 -0500

    entity & dbcontext

---
+++ PROG3176_Assignment2/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROG3176_Assignment2.Entities;
+
+namespace PROG3176_Assignment2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
---
+++ PROG3176_Assignment2/Entities/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG3176_Assignment2.Entities
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Species { get; set; }
+
+        [Required]
+        public int? Age { get; set; }
+
+        public bool IsPet { get; set; } = false;
+    }
+}
