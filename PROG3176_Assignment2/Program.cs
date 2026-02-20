326ea7175b027879bd62a19850708ab5b09b9aa9
TeaLizard <owentwilliams7@gmail.com>
2026-02-20 00:30:12 -0500

    two db solution

--- PROG3176_Assignment2/Program.cs
+++ PROG3176_Assignment2/Program.cs
@@ -4,8 +4,18 @@ using PROG3176_Assignment2.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite("Data Source=TestDb.db"));
+}
+else
+{
+    var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
 
 builder.Services.AddScoped<AnimalRepository>();
 
